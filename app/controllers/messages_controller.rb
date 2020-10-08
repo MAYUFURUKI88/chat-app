9cb84960fd5f800b002ae6777ec7b1b7c3e02250
Mayuu Furuki <aaa08027744484@gmail.com>
2020-10-08 16:28:21 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -22,4 +22,6 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id.current_user.id)
 end
